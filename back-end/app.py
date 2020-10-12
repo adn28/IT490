1ec39745fbc19f18e4446949f9b0cd8d7c6d7826
yash225 <60192150+yash225@users.noreply.github.com>
2020-10-11 20:40:52 -0400

    Milestone 3

--- back-end/app.py
+++ back-end/app.py
@@ -1,13 +1,20 @@
 import pika
+import psycopg2
 import time
+import os
 
-time.sleep(20)
-
-print("Connecting to messaging service...")
-credentials = pika.PlainCredentials('guest','guest')
-connection = pika.BlockingConnection(
-	pika.ConnectionParameters(
-        	host='messaging',
-		credentials=credentials
-  )
+postgres_password = os.environ['POSTGRES_PASSWORD']
+con = psycopg2.connect(
+    host='database',
+    databast='example',
+    user='postgres',
+    password=postgres_password  
 )
+
+cur = con.cursor()
+
+cur.execute("CREATE TABLE Products(Id INTEGER PRIMARY KEY, Name VARCHAR(20), Price INT)")
+
+
+con.commit()
+con.close()  
--- front-end/app.py
+++ front-end/app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-	return 'Hello World, we have Flask in a Docker container!'
+     return 'Hello World, we have flask in a Docker container!'
+
+@app.route('/login')
+def login():
+     return render_template('login.html');
+
+@app.route('/register')
+def register():
+     return render_template('register.html');
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
